49a685c96f93d2c1aed8a1657b12d2f9a70b9871
Alan Couto <alancoutorock@gmail.com>
2025-08-05 13:03:25 -0300

    Atualiza o arquivo tests/test_main.py  com nova lógica para executar ZeroDivisionError

--- devops/tests/test_main.py
+++ devops/tests/test_main.py
@@ -1,8 +1,13 @@
 # tests/test_main.py
-
+import pytest
 from app.main import teorema_bayes
 
 def test_teorema_bayes():
     assert teorema_bayes(2, 3, 2, 2) == 0.6
     assert teorema_bayes(-1, 1, -1, 1) == 0.5
-    assert teorema_bayes(0, 0, 0, 0) == ZeroDivisionError
+    
+    with pytest.raises(ZeroDivisionError):
+        teorema_bayes(0, 0, 0, 0)
+
+
+print(test_teorema_bayes)
