0c12db5c035929b2bbd548ff6bcdcf8eb4a86c03
Alan Couto <alancoutorock@gmail.com>
2025-08-16 12:20:55 -0300

    main.tf novo

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,8 @@
-#infra\main.tf
 provider "aws" {
-  region                  = var.aws_region
-  access_key              = var.aws_access_key
-  secret_key              = var.aws_secret_key
-  token                   = var.aws_session_token
+  region          = var.aws_region
+  access_key      = var.aws_access_key
+  secret_key      = var.aws_secret_key
+  token           = var.aws_session_token
 }
 
 resource "aws_instance" "teorema_bayes_ec2" {
@@ -18,12 +17,64 @@ resource "aws_instance" "teorema_bayes_ec2" {
 
   user_data = <<-EOF
               #!/bin/bash
+              set -e  # O 'set -e' faz o script parar em caso de erro
+
+              # Log de inicialização para depuração
+              echo "Iniciando script user_data" > /tmp/user_data.log
+
+              # Atualiza os pacotes e instala as dependências
               apt-get update -y
               apt-get install -y python3 python3-pip git
+              echo "Dependências instaladas." >> /tmp/user_data.log
+
+              # Navega para o diretório de destino e clona o repositório
               cd /home/ubuntu
               git clone https://github.com/Alan-Couto-Projetos/devops-python
+              echo "Repositório clonado." >> /tmp/user_data.log
+
+              # Navega para o diretório do repositório
               cd devops-python
+              
+              # Instala as dependências do Python
               pip3 install -r requirements.txt
+              echo "Dependências do Python instaladas." >> /tmp/user_data.log
+
+              # Executa o script Python
               python3 app/main.py > resultado.txt
+              echo "Script Python executado com sucesso." >> /tmp/user_data.log
+
+              echo "Script user_data finalizado." >> /tmp/user_data.log
               EOF
+}
+
+#infra/outputs.tf
+output "public_ip" {
+  description = "IP público da instância EC2"
+  value       = aws_instance.teorema_bayes_ec2.public_ip
+}
+
+#infra/variables.tf
+variable "aws_region" {
+  description = "Região da AWS"
+  type        = string
+}
+
+variable "aws_access_key" {
+  description = "Chave pública temporária"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "Chave secreta temporária"
+  type        = string
+}
+
+variable "aws_session_token" {
+  description = "Token temporário do AWS Academy"
+  type        = string
+}
+
+variable "key_name" {
+  description = "O nome do par de chaves SSH para a instância EC2."
+  type        = string
 }
