90db7e5022b2e2421708838901f450370651dbfd
Alan Couto <alancoutorock@gmail.com>
2025-08-16 12:42:09 -0300

    main.tf novo 3

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,8 @@
-#infra\main.tf
 provider "aws" {
-  region                  = var.aws_region
-  access_key              = var.aws_access_key
-  secret_key              = var.aws_secret_key
-  token                   = var.aws_session_token
+  region                      = var.aws_region
+  access_key                  = var.aws_access_key
+  secret_key                  = var.aws_secret_key
+  token                       = var.aws_session_token
 }
 
 resource "aws_instance" "teorema_bayes_ec2" {
@@ -18,33 +17,34 @@ resource "aws_instance" "teorema_bayes_ec2" {
 
   user_data = <<-EOF
               #!/bin/bash
-              set -e  # O 'set -e' faz o script parar em caso de erro
+              set -e
 
-              # Log de inicialização para depuração
-              echo "Iniciando script user_data" > /tmp/user_data.log
-
-              # Atualiza os pacotes e instala as dependências
+              # Atualiza os pacotes e instala as dependências como root
               apt-get update -y
               apt-get install -y python3 python3-pip git
-              echo "Dependências instaladas." >> /tmp/user_data.log
-
-              # Navega para o diretório de destino e clona o repositório
-              cd /home/ubuntu
-              git clone https://github.com/Alan-Couto-Projetos/devops-python
-              echo "Repositório clonado." >> /tmp/user_data.log
-
-              # Navega para o diretório do repositório
-              cd devops-python
               
-              # Instala as dependências do Python
-              pip3 install -r requirements.txt
-              echo "Dependências do Python instaladas." >> /tmp/user_data.log
-
-              # Executa o script Python
-              python3 app/main.py > resultado.txt
-              echo "Script Python executado com sucesso." >> /tmp/user_data.log
-
-              echo "Script user_data finalizado." >> /tmp/user_data.log
+              # Troca para o usuário 'ubuntu' para operações de clonagem e execução
+              # isso garante as permissões corretas
+              su - ubuntu -c "
+                echo 'Iniciando script user_data' > /home/ubuntu/user_data.log
+                
+                # Navega para o diretório de usuário
+                cd /home/ubuntu
+                
+                # Clona o repositório e instala dependências
+                git clone https://github.com/Alan-Couto-Projetos/devops-python
+                echo 'Repositório clonado.' >> /home/ubuntu/user_data.log
+                
+                cd devops-python
+                
+                pip3 install -r requirements.txt
+                echo 'Dependências do Python instaladas.' >> /home/ubuntu/user_data.log
+                
+                # Executa o script Python
+                python3 devops/app/main.py > resultado.txt
+                echo 'Script Python executado com sucesso.' >> /home/ubuntu/user_data.log
+              "
+              
+              echo "Script user_data finalizado com sucesso." >> /home/ubuntu/user_data.log
               EOF
 }
-
